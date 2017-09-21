9c9ccbbea77b64bef26d8b733e77913fcdac6b1f
LeeScofield <175525316@qq.com>
2017-09-21 22:41:32 +0800

    branch1第一次提交

--- src/main/java/com/test/git/App.java
+++ src/main/java/com/test/git/App.java
@@ -2,6 +2,7 @@ package com.test.git;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Hello world!
@@ -16,5 +17,8 @@ public class App
 
         list.sort((a,b) -> a - b);
         list.forEach(System.out::println);
+
+        Function<String,String> fun = (a) -> "你好:" + a;
+        System.out.println(fun.apply("张三"));
     }
 }
